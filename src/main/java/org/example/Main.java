6484909d731727dd548d0346f2a91482621ce19b
wil250b <143456387+wil250b@users.noreply.github.com>
2024-04-22 14:54:11 -0400

    This is a test message

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+    }
+}
