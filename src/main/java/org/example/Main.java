72b2861b6a19ec21050a03167ab4f8ff1a01a106
wil250b <143456387+wil250b@users.noreply.github.com>
2024-04-22 15:13:45 -0400

    added a line to print a line

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         //and some extra text
+        System.out.println("new line");
     }
 }
